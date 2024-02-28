41786338aee47c59e08ed6a7e03fd9a3b715c54e
Rosano Coutinho <github.com@xeari.com>
2024-02-28 10:46:57 -0500

    redirect Ghost

--- main.js
+++ main.js
@@ -93,5 +93,50 @@ app.use(function redirectPeerTube (req, res, next) {
   return res.redirect(301, match || 'https://www.youtube.com/channel/UCPMFNN-2JUuS6D9iYfrVK8g');
 });
 
+app.use(function redirectGhost (req, res, next) {
+  if (!req.get('host').match('ephemerata.rosano.ca')) {
+    return next();
+  }
+
+  const map = {
+    'feed': 'feed',
+    'rss': 'rss',
+    'let-time-work-for-you': 'let-time-work-for-you',
+    '2022': '2022',
+    'ive-never-worked-for-a-company': 'ive-never-worked-in-a-company',
+    '01gfggykmr5b5fhm3a0ghv4z1y': 'thirty-four',
+    '01gdmrksee48hte06mv436emf6': 'how-i-build-a-digital-project-archive',
+    '01fvmvwv1p0te9nh86z4r2h82x': 'applying-note-taking-reflexes-to-making-music',
+    '01fsf9z4d1yq29sk00y32w1ynm': 'what-we-want',
+    '01fra43ge7yaj2rgv2cya33q46': '2021',
+    '01fqhhwczcs76rgy4fqestpmdb': 'secular-churches-for-continuity',
+    '01fpxjtkv33xawr09px2ftppr9': 'why-i-added-text-to-my-pure-icon-site-after-eight-years',
+    '01fpp2xb6fe3xbpswvfc4pxbmq': 'platform-puzzle-pieces-for-sustainable-community',
+    '01fp3ke8f9z7adc6gbe1pdyyse': 'evolution-one',
+    '01fmkad2j6gtxtx0hhedse2s3k': 'dating-apps-take-one',
+    '01eyk3k2fazfza5rceyvbdb8n6': 'sixth-times-a-charm',
+    '01eyk6mwbw9g2nb1dht0ffkqza': 'coming-to-the-guitar-later-in-life',
+    '01eyy9nhtzmx74zqn4gp55nyqp': 'learning-portuguese',
+    '01f1wjp6bb4raqdqd4aanb0evn': 'teaching-languages-to-friends',
+    '01f5cfkdd1x912wsyer3099bkr': 'my-mothers-gift',
+    '01f6srp7b67f2pe06bk7dqk9y1': 'six-guns-pointed-at-my-face',
+    '01fawxsfwn9erqg2c2mz3psycs': 'inner-feedback-loops',
+    '01fhvazke1y4bqfd883c9t9cm5': 'thirty-three',
+    '01evv3hq1ak4b6ng1jzppx5n2j': 'going-doorless',
+    '01fcnd5gyp3nw6yhecz6q94rvg': 'family-language-exchange',
+    '01fj7k48m0915q7rcc7jcb1v3q': 'rethinking-analytics',
+    '01fh30m6w0njmbbt4jayzyr2yq': 'events-are-work',
+    '01fj75ct40ajy45tqhafdbm3tm': 'community-essence',
+    '01fk3v8d7s6jw0wbhp3ck7xkyf': 'wetware-of-writing-and-doing',
+    '01fmeehzvr3n9q0rkrnf7y2d5c': 'going-fully-web',
+  };
+  const match = (Object.entries(map).filter(function ([key, value]) {
+    return key.split(',').filter(function (e) {
+      return req.url.match(e);
+    }).length;
+  }).pop() || []).pop();
+
+  return res.redirect(301, 'https://utopia.rosano.ca/' + (match || ''));
+});
 
 app.listen(process.env.PORT);
