3ed596df0fb4cebe5d32c231b4aecd44de091bbd
Rosano Coutinho <github.com@xeari.com>
2024-02-28 10:45:50 -0500

    remove www

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const app = require('express')();
 
+app.use(function removeWWW (req, res, next) {
+  if (req.headers.host.slice(0, 4) !== 'www.') {
+    return next();
+  }
+
+  return res.redirect(301, 'https://' + req.headers.host.slice(4) + req.originalUrl);
+});
+
 
 app.listen(process.env.PORT);
