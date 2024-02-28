52bf5d6ffe8dde9faadc0d71a524d76ec70be8ee
Rosano Coutinho <github.com@xeari.com>
2024-02-28 10:46:04 -0500

    correct domain

--- main.js
+++ main.js
@@ -8,5 +8,21 @@ app.use(function removeWWW (req, res, next) {
   return res.redirect(301, 'https://' + req.headers.host.slice(4) + req.originalUrl);
 });
 
+app.use(function correctDomain (req, res, next) {
+  const map = {
+    'oott.rosano.ca': 'archive.rosano.ca',
+    'refs.rosano.ca': 'ref.rosano.ca',
+    'rcreativ.com': 'rosano.ca',
+    'phileplanet.com': 'rosano.ca',
+    'list.rosano.ca': 'utopia.rosano.ca',
+  };
+
+  if (!Object.keys(map).includes(req.get('host'))) {
+    return next();
+  }
+
+  return res.redirect(301, 'https://' + map[req.get('host')] + req.originalUrl);
+});
+
 
 app.listen(process.env.PORT);
